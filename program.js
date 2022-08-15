ab60eb2ce50509db601e9c13b47ce1c87930dd29
BoomBakar <abubakardar608@gmail.com>
2022-08-15 19:04:18 +0500

    First commit

---
+++ program.js
@@ -0,0 +1,34 @@
+
+
+// //The average of even integers from 2 to 100
+// var sum = 0;
+// var count = 0;
+// for (var i = 2; i <= 100; i+=2) {
+//     sum += i;
+//     count++;
+
+// }
+// var average = sum / count;
+// console.log(average);
+
+// //The average of odd integers from 2 to 100
+// var sum = 0;
+// var count = 0;
+// for (var i = 2; i <= 100; i++) {
+//     if (i % 2 !== 0) {
+//         sum += i;
+//         count++;
+//     }
+// }
+// var average = sum / count;
+// console.log(average);
+
+
+// //valid password regular expression
+// var password = "Dar!.12345";
+// var regex = /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{8,}$/; //^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{8,}$ 
+// console.log(regex.test(password));
+
+
+
+
